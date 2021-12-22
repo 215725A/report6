065fb541cce3b6c6201aff5d9979b504a5cab429
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2021-12-22 23:39:14 +0900

    make Add method

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcAdd.java
@@ -0,0 +1,34 @@
+package jp.ac.uryukyu.ie.e215725.Calclator;
+
+import jp.ac.uryukyu.ie.e215725.Settings.Calc;
+
+public class CalcAdd implements Calc{
+    public double value1;
+    public double value2;
+
+    /** 
+     * コンストラクター
+     * @param _value1 引数1
+     * @param _value2 引数2
+     */
+    public CalcAdd(double _value1, double _value2){
+        this.value1 = _value1;
+        this.value2 = _value2;
+    }
+
+    /**
+     * 計算結果を出力する
+     */
+    @Override
+    public void calc(){
+        System.out.println(calcAdd());
+    }
+
+    /**
+     * 値1と値2の足し算を行うメソッド
+     * @return 値1と値2の和
+     */
+    public double calcAdd(){
+        return this.value1 + this.value2;
+    }
+}
