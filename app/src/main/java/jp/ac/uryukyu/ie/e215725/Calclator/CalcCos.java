cb8c02dba492282b1062fd119246aeaaecd62377
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2021-12-23 01:34:26 +0900

    make Cos method

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcCos.java
@@ -0,0 +1,35 @@
+package jp.ac.uryukyu.ie.e215725.Calclator;
+
+import jp.ac.uryukyu.ie.e215725.Settings.Calc;
+
+public class CalcCos extends Trigonometric implements Calc{
+    public double cos;      //コサイン
+
+    /**
+     * コンストラクタ
+     * スーパークラス(Trigonometric)を継承
+     * @param _theta
+     */
+    public CalcCos(double _theta){
+        super(_theta);
+    }
+
+    /**
+     * calcメソッドのオーバーライド
+     * Cosを求めるメソッドの実行結果を出力する
+     */
+    @Override
+    public void calc(){
+        System.out.println(calcCos());
+    }
+
+    /**
+     * Cosを求めるメソッド
+     * @return cos(radian)の値
+     */
+    public double calcCos(){
+        cos = Math.cos(this.theta);
+        return cos;
+    }
+    
+}
