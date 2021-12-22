95893b595c377c55eef1689df92042dccb907646
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2021-12-22 23:39:42 +0900

    make Add method

--- app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcAdd.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcAdd.java
@@ -3,8 +3,8 @@ package jp.ac.uryukyu.ie.e215725.Calclator;
 import jp.ac.uryukyu.ie.e215725.Settings.Calc;
 
 public class CalcAdd implements Calc{
-    public double value1;
-    public double value2;
+    public double value1;       //足される数
+    public double value2;       //足す数
 
     /** 
      * コンストラクター
