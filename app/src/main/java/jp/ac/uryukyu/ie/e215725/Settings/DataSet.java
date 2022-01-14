a6dba51e530769ae8247bc5d868edb6955c89f20
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2022-01-15 03:23:25 +0900

    Add document

--- app/src/main/java/jp/ac/uryukyu/ie/e215725/Settings/DataSet.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Settings/DataSet.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DataSet {
+    /**
+     * 不動小数点型のデータリストを作成するメソッド
+     * @param doubleData 空のデータリスト
+     * @return 不動小数点型のデータリスト
+     */
     public static ArrayList<Double> dataSetDouble(ArrayList<Double> doubleData) {
         Scanner scan = new Scanner(System.in);
         System.out.println("データの数は何個ですか");
