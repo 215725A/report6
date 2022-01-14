e9c05415e4e8e1f934d1ad9eec5e2ff60baebf44
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2022-01-15 01:22:48 +0900

    Can calc more two datas

--- app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcDivision.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/CalcDivision.java
@@ -1,36 +1,35 @@
 package jp.ac.uryukyu.ie.e215725.Calclator;
 
 import jp.ac.uryukyu.ie.e215725.Settings.Calc;
+import java.util.ArrayList;
 
-public class CalcDivision implements Calc{
-    public double value1;       //割られる数
-    public double value2;       //割る数
+public class CalcDivision extends Calc{
+    public ArrayList<Double> doubleData; //除算したい数のリスト
 
     /**
      * コンストラクタ
      * @param _value1 ユーザーが入力した割られる数
      * @param _value2 ユーザーが入力した割る数
      */
-    public CalcDivision(double _inputvalue1, double _inputvalue2){
-        this.value1 = _inputvalue1;
-        this.value2 = _inputvalue2;
+    public CalcDivision(ArrayList<Double> _doubleData){
+        this.doubleData = _doubleData;
     }
 
     /**
      * calcメソッドのオーバーライド
-     * 割り算を行うメソッドを呼び出して結果を出力する
+     * リストの中身を割り算していく
+     * 最初の要素から割っていく
+     * 初期値を最初の要素にする(0を割っても0になるため)
      */
     @Override
     public void calc(){
-        System.out.println(calcDivision());
-    }
-
-    /**
-     * 割り算を行うメソッド
-     * @return フィールド変数value1 を value2で割った値を返す
-     */
-    public double calcDivision(){
-        return this.value1 / this.value2;
+        for(int index = 0; index < doubleData.size(); index++){
+            if(index == 0){
+                resultOfDouble = doubleData.get(index);
+            }else{
+                resultOfDouble /= doubleData.get(index);
+            }
+        }
     }
     
 }
