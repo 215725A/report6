a8f8846184ce2ed3e93a177960b59ab4e6d03b02
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2022-01-15 02:40:06 +0900

    Remove class field

--- app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/Trigonometric.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215725/Calclator/Trigonometric.java
@@ -1,16 +1,18 @@
 package jp.ac.uryukyu.ie.e215725.Calclator;
 
 public class Trigonometric {
-    public double theta;    //角度
-    public double radian;   //ラジアン
 
     /**
      * コンストラクタ
      * 入力された角度をラジアンに変換する
      * @param _theta
      */
-    public Trigonometric(double _theta){
-        this.theta = _theta;
-        this.radian = Math.toRadians(this.theta);
+    // public Trigonometric(double _theta){
+    //     this.theta = _theta;
+    //     this.radian = Math.toRadians(this.theta);
+    // }
+
+    public static double changeToRadian(double theta){
+        return Math.toRadians(theta);
     }
 }
